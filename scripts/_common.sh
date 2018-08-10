626f858b2efc568a24b4a6d55d5e4fe3fe9f8577
Jean-Baptiste Holcroft <jean-baptiste@holcroft.fr>
2018-08-10 17:27:41 +0200

    use ynh_setup_source instead of git

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,12 +3,6 @@
 # Common variables
 #
 
-# Git repository of Movim
-GIT_REPO="https://github.com/movim/movim"
-
-# Commit to checkout
-HEAD_COMMIT="ee249f1e37b548c71b9a339dd358fbd2136e3291"
-
 # App package root directory should be the parent folder
 PKGDIR=$(cd ../; pwd)
 
@@ -23,11 +17,11 @@ exec_cmd() {
    && sudo sudo -u movim "$@")
 }
 
-# Apply the SSO patch to Movim source code
-# usage: apply_sso_patch
-apply_sso_patch() {
-  local patch_path="/tmp/sso-logout.patch"
-  cp -f "${PKGDIR}/patches/sso-logout.patch" "$patch_path"
-  exec_cmd git apply "$patch_path"
-  ynh_secure_remove "$patch_path"
+# Undo the SSO patch to Movim source code (applied by default with ynh_setup_source)
+# usage: undo_sso_patch
+undo_sso_patch() {
+  	(
+  		cd $final_path
+		patch -p1 -R < $YNH_CWD/../sources/patches/app-00-sso-logout.patch
+	)
 }
