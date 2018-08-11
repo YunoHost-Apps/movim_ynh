5504deffe2ddb9db5fcb23451cfcd78cae7a9f0a
Jean-Baptiste Holcroft <jean-baptiste@holcroft.fr>
2018-08-12 00:59:39 +0200

    Remove exec_cm and cleanups

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -10,14 +10,7 @@ PKGDIR=$(cd ../; pwd)
 # Common helpers
 #
 
-# Execute a command as movim user in the destination directory
-# usage: exec_cmd COMMAND [ARG ...]
-exec_cmd() {
-  (cd "$final_path" \
-   && sudo sudo -u movim "$@")
-}
-
-# Undo the SSO patch to Movim source code (applied by default with ynh_setup_source)
+# Undo the SSO patch to source code (applied by default with ynh_setup_source)
 # usage: undo_sso_patch
 undo_sso_patch() {
   	(
