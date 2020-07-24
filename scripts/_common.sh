5067cf915a3d4c67a953c2ce715039951925e7bc
ericgaspar <junk.eg@free.fr>
2020-07-24 18:49:44 +0200

    fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="postgresql apt-transport-https composer"
+pkg_dependencies="postgresql postgresql-contrib apt-transport-https composer"
 
 extra_php_dependencies="php${YNH_PHP_VERSION}-fpm php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-pgsql php${YNH_PHP_VERSION}-xml"
 
