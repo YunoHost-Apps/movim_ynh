e23e0e2df70aa75c09711d474f272fcf34773a44
ericgaspar <junk.eg@free.fr>
2020-09-16 21:18:58 +0200

    git

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,9 +8,10 @@
 pkg_dependencies="postgresql postgresql-contrib apt-transport-https"
 #composer
 
+YNH_PHP_VERSION="7.3"
+
 extra_php_dependencies="php${YNH_PHP_VERSION}-fpm php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-pgsql php${YNH_PHP_VERSION}-xml"
 
-YNH_PHP_VERSION="7.3"
 
 #=================================================
 # PERSONAL HELPERS
