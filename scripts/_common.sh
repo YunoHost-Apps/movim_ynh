96689d7a4446e0110cf20b1858f3921e11ff46f4
ericgaspar <junk.eg@free.fr>
2020-09-17 10:25:34 +0200

    fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -24,3 +24,16 @@ extra_php_dependencies="php${YNH_PHP_VERSION}-fpm php${YNH_PHP_VERSION}-curl php
 #=================================================
 # FUTURE OFFICIAL HELPERS
 #=================================================
+
+# Execute a command as another user
+# usage: ynh_exec_as USER COMMAND [ARG ...]
+ynh_exec_as() {
+  local USER=$1
+  shift 1
+
+  if [[ $USER = $(whoami) ]]; then
+    eval "$@"
+  else
+    sudo -u "$USER" "$@"
+  fi
+}
