95575db836b59bdfe91d6c63a01e2c1f0b791c1c
ericgaspar <junk.eg@free.fr>
2022-01-15 09:31:05 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,7 @@
 # dependencies used by the app
 YNH_PHP_VERSION="7.3"
 
-YNH_COMPOSER_VERSION="2.2.4"
+YNH_COMPOSER_VERSION="2.0.13"
 
 pkg_dependencies="postgresql postgresql-contrib apt-transport-https php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-pgsql php${YNH_PHP_VERSION}-xml"
 
