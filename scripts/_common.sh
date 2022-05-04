3367881b2df621b44eab025829382923ae288166
jarod5001 <68397534+jarod5001@users.noreply.github.com>
2022-05-04 14:27:17 +0200

    php8.0/composer2.3.5

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,9 +4,9 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION="7.3"
+YNH_PHP_VERSION="8.0"
 
-YNH_COMPOSER_VERSION="2.2.5"
+YNH_COMPOSER_VERSION="2.3.5"
 
 pkg_dependencies="postgresql postgresql-contrib apt-transport-https php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-pgsql php${YNH_PHP_VERSION}-xml"
 
