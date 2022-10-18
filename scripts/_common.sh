cfe2e5e5bb23efbceac25afaf5c96fefc9ad0794
ericgaspar <junk.eg@free.fr>
2022-10-18 21:26:38 +0200

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,9 +4,9 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION="7.4"
+YNH_PHP_VERSION="8.0"
 
-YNH_COMPOSER_VERSION="2.2.5"
+YNH_COMPOSER_VERSION="2.4.3"
 
 php_dependencies="php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-pgsql php${YNH_PHP_VERSION}-xml"
 
