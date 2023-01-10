ad6b02a3739c4d3d080d940516fe3e263d1ee5a3
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-01-10 12:10:52 +0100

    Fix PHP

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,11 +4,13 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION="7.3"
+YNH_PHP_VERSION="7.4"
 
 YNH_COMPOSER_VERSION="2.2.5"
 
-pkg_dependencies="postgresql postgresql-contrib apt-transport-https php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-pgsql php${YNH_PHP_VERSION}-xml"
+php_dependencies="php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-pgsql php${YNH_PHP_VERSION}-xml"
+
+pkg_dependencies="postgresql postgresql-contrib apt-transport-https $php_dependencies"
 
 #=================================================
 # PERSONAL HELPERS
