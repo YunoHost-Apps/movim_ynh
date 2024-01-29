010590a5e40083d08652ce14834afcaafb72fe00
Félix Piédallu <felix@piedallu.me>
2024-01-29 17:21:02 +0100

    Packaging v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,13 +4,7 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION="7.4"
-
-YNH_COMPOSER_VERSION="2.2.5"
-
-php_dependencies="php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-pgsql php${YNH_PHP_VERSION}-xml"
-
-pkg_dependencies="postgresql postgresql-contrib apt-transport-https $php_dependencies"
+composer_version="2.2.5"
 
 #=================================================
 # PERSONAL HELPERS
