5c5da60c0d952eef4b5a671368ccc1032a18f71e
Félix Piédallu <felix@piedallu.me>
2024-03-24 13:32:37 +0100

    Fix php version with a dummy path bin dir

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -10,6 +10,13 @@ YNH_COMPOSER_VERSION="2.4.3"
 # PERSONAL HELPERS
 #=================================================
 
+_fix_php_version() {
+    mkdir -p "$install_dir/phppath"
+    ln -s "/usr/bin/php$phpversion" "$install_dir/phppath/php"
+    PATH="$install_dir/phppath:$PATH" "$@"
+    ynh_secure_remove --file="$install_dir/phppath"
+}
+
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
